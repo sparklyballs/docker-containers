b7fca084350944d7c665edb69d2795bb21542816
sparklyballs <sparklyballs@gmail.com>
2015-03-03 22:03:46 +0000

    Update firstrun.sh

--- cyca/firstrun.sh
+++ cyca/firstrun.sh
@@ -4,6 +4,6 @@ echo "using existing website"
 chown -R www-data:www-data /var/log/apache2
 else
 echo "fetching cyca files"
-cp -pr cd /root/tempwww/Cyca /var/www/Cyca
+cp -pr cd /root/tempwww/Cyca /var/www/
 chown -R www-data:www-data /var/www/Cyca
 fi
