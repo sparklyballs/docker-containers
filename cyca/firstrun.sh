37e69589c34e1bbf55d75024d3a1bf34297e8d77
sparklyballs <sparklyballs@gmail.com>
2015-03-08 02:23:30 +0000

    Update firstrun.sh

--- cyca/firstrun.sh
+++ cyca/firstrun.sh
@@ -2,10 +2,13 @@
 if [ -f "/var/www/Cyca/index.php" ]; then
 echo "using existing website"
 chown -R www-data:www-data /var/log/apache2
+cd /var/www/Cyca
+GIT_SSL_NO_VERIFY=true git pull
+chown -R www-data:www-data /var/www/Cyca
 else
 echo "fetching cyca files"
 cp -pr /root/tempwww/Cyca /var/www/
 sleep 8
 chown -R www-data:www-data /var/www/Cyca
 fi
-crontab /root/cronjob
+
