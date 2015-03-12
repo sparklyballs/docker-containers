75aac56144238b663ee2c30f79decb702cfd9ba5
sparklyballs <sparklyballs@gmail.com>
2015-03-12 13:49:55 +0000

    Update firstrun.sh

--- cyca/firstrun.sh
+++ cyca/firstrun.sh
@@ -2,8 +2,6 @@
 if [ -f "/var/www/Cyca/index.php" ]; then
 echo "using existing website"
 chown -R www-data:www-data /var/log/apache2
-cd /var/www/Cyca
-GIT_SSL_NO_VERIFY=true git pull
 chown -R www-data:www-data /var/www/Cyca
 else
 echo "fetching cyca files"
