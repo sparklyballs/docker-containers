e4004d69f34f8d22f130aecc6d60666db7bccb1c
sparklyballs <sparklyballs@gmail.com>
2015-03-13 02:49:34 +0000

    Create firstrun.sh

---
+++ Myyna/src/firstrun.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ -f "/var/www/myyna/app.js" ]; then
+echo "using existing website"
+chown -R www-data:www-data /var/log/apache2
+chown -R www-data:www-data /var/www/myyna
+else
+echo "fetching cyca files"
+cp -pr /root/myyna /var/www/
+sleep 15
+chown -R www-data:www-data /var/www/myyna
+fi
