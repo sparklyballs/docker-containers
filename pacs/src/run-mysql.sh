3a3d0586dee7d5c2d746ff74a6905504066e38e8
sparklyballs <sparklyballs@gmail.com>
2015-03-19 19:40:36 +0000

    Update run-mysql.sh

--- pacs/src/run-mysql.sh
+++ pacs/src/run-mysql.sh
@@ -19,6 +19,13 @@ else
   echo "Database created. Granting access to 'root' ruser for all hosts."
   mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION"
   mysqladmin -u root shutdown
+  /usr/bin/mysqld_safe --skip-syslog --datadir='/DATABASE' &
+sleep 5s
+mysql -uroot > /root/databases.sql
+mysql -upacs -ppacs pacsdb < /DICOM/dcm4chee/dcm4chee-2.17.1-mysql/sql/create.mysql
+mysql -uarr -parr arrdb < /DICOM/dcm4chee/dcm4chee-arr-3.0.11-mysql/sql/dcm4chee-arr-mysql.ddl
+killall mysqld
+sleep 5s
   chown -R nobody:users /DATABASE
   chmod -R 755 /DATABASE
 fi
