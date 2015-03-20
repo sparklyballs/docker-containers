881d5b093565fe7e69cbc5bc5736dedc3fb018ef
sparklyballs <sparklyballs@gmail.com>
2015-03-20 19:36:15 +0000

    tweaking pacs

--- pacs/src/firstrun.sh
+++ pacs/src/firstrun.sh
@@ -14,10 +14,12 @@ if [ -d "/DATABASE/arrdb" ]; then
 echo "Using existing databases"
 chown -R nobody:users /DATABASE
 /usr/bin/supervisord
+sleep 20s
 else
 echo "new setup, copying empty databases"
 cp -pr /var/lib/mysql/* /DATABASE/
 chown -R nobody:users /DATABASE
 sleep 10s
 /usr/bin/supervisord
+sleep 20s
 fi
