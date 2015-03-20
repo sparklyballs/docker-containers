77291fb60be1fcb366593b1474f9b425e494f552
sparklyballs <sparklyballs@gmail.com>
2015-03-20 01:31:15 +0000

    Update firstrun-mysql.sh

--- pacs/src/firstrun-mysql.sh
+++ pacs/src/firstrun-mysql.sh
@@ -3,13 +3,13 @@ if [ -f "/DATABASE/arrdb/active_part.frm" ]; then
 echo "Using existing databases"
 chown -R nobody:users /DATABASE
 else
-cp -pr /var/lib/mysql/* /DATABASE
+cp -pr /var/lib/mysql/* /DATABASE/
 chown -R nobody:users /DATABASE
 mysql_install_db
-/usr/bin/mysqld_safe &
+/usr/bin/mysqld_safe --skip-syslog
 sleep 5s
 # Create the 'pacsdb' and 'arrdb' databases, and 'pacs' and 'arr' DB users.
-mysql -uroot < /root/databases.sql
+mysql -unobody < /root/databases.sql
 # Load the 'pacsdb' database schema
 mysql -upacs -ppacs pacsdb < /root/create.mysql
 # Load the 'arrdb' database schema
