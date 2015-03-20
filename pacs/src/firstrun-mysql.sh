2af3f9a817fd7f9b884a4ea2eb43b6c0b7bc57d7
sparklyballs <sparklyballs@gmail.com>
2015-03-20 00:15:50 +0000

    fix up create database scripts pacs

--- pacs/src/firstrun-mysql.sh
+++ pacs/src/firstrun-mysql.sh
@@ -5,8 +5,8 @@ sleep 5s
 # Create the 'pacsdb' and 'arrdb' databases, and 'pacs' and 'arr' DB users.
 mysql -uroot < /root/databases.sql
 # Load the 'pacsdb' database schema
-mysql -upacs -ppacs pacsdb < /DICOM/dcm4chee/dcm4chee-2.17.1-mysql/sql/create.mysql
+mysql -upacs -ppacs pacsdb < /root/create.mysql
 # Load the 'arrdb' database schema
-mysql -uarr -parr arrdb < /DICOM/dcm4chee/dcm4chee-arr-3.0.11-mysql/sql/dcm4chee-arr-mysql.ddl
+mysql -uarr -parr arrdb < /root/dcm4chee-arr-mysql.ddl
 killall mysqld
 sleep 5s
