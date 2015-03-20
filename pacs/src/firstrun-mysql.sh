c3da05cc2c9e686a229a9fcea929b0e1fe5d8004
sparklyballs <sparklyballs@gmail.com>
2015-03-20 10:28:15 +0000

    add mv database pacs

--- pacs/src/firstrun-mysql.sh
+++ pacs/src/firstrun-mysql.sh
@@ -3,17 +3,8 @@ if [ -f "/DATABASE/arrdb/active_part.frm" ]; then
 echo "Using existing databases"
 chown -R nobody:users /DATABASE
 else
+echo "new setup, copying empty databases"
+cp -pr /var/lib/mysql/* /DATABASE/
 chown -R nobody:users /DATABASE
-mysql_install_db
-/usr/bin/mysqld_safe &
-sleep 5s
-# Create the 'pacsdb' and 'arrdb' databases, and 'pacs' and 'arr' DB users.
-mysql -uroot < /root/databases.sql
-# Load the 'pacsdb' database schema
-mysql -upacs -ppacs pacsdb < /root/create.mysql
-# Load the 'arrdb' database schema
-mysql -uarr -parr arrdb < /root/dcm4chee-arr-mysql.ddl
-killall mysqld
-sleep 5s
-chown -R nobody:users /DATABASE
+sleep 10s 
 fi
