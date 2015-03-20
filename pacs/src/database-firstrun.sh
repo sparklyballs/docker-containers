08a5ae83250c88b39114428afd875d574ed7406c
sparklyballs <sparklyballs@gmail.com>
2015-03-20 18:43:46 +0000

    try supervisor for pacs mysql

--- pacs/src/database-firstrun.sh
+++ pacs/src/database-firstrun.sh
@@ -3,11 +3,11 @@ rm -rf /var/local/mysql/*.pid
 if [ -d "/DATABASE/arrdb" ]; then
 echo "Using existing databases"
 chown -R nobody:users /DATABASE
-/usr/bin/mysqld_safe --skip-syslog &
+/usr/bin/supervisord
 else
 echo "new setup, copying empty databases"
 cp -pr /var/lib/mysql/* /DATABASE/
 chown -R nobody:users /DATABASE
 sleep 10s 
-/usr/bin/mysqld_safe --skip-syslog &
+/usr/bin/supervisord
 fi
