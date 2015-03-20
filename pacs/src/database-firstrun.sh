e24d1f17921417b3ae83e631395138ed0865c1e1
sparklyballs <sparklyballs@gmail.com>
2015-03-20 13:58:34 +0000

    Update database-firstrun.sh

--- pacs/src/database-firstrun.sh
+++ pacs/src/database-firstrun.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 rm -rf /var/local/mysql/*.pid
-if [ -f "/DATABASE/arrdb/active_part.frm" ]; then
+if [ -d "/DATABASE/arrdb" ]; then
 echo "Using existing databases"
 chown -R nobody:users /DATABASE
+/usr/bin/mysqld_safe --skip-syslog &
 else
 echo "new setup, copying empty databases"
 cp -pr /var/lib/mysql/* /DATABASE/
 chown -R nobody:users /DATABASE
 sleep 10s 
+/usr/bin/mysqld_safe --skip-syslog &
 fi
-/usr/bin/mysqld_safe --skip-syslog
+
