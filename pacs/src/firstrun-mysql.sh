c5176d607286964a6db942fad2e5601cd8a0760a
sparklyballs <sparklyballs@gmail.com>
2015-03-20 00:42:50 +0000

    more fix up changes pacs

--- pacs/src/firstrun-mysql.sh
+++ pacs/src/firstrun-mysql.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ -f "/DATABASE/arrdb/active_part.frm" ]; then
+echo "Using existing databases"
+chown -R nobody:users /DATABASE
+else
 cp -pr /var/lib/mysql/* /DATABASE
 chown -R nobody:users /DATABASE
 mysql_install_db
@@ -10,6 +14,8 @@ mysql -uroot < /root/databases.sql
 mysql -upacs -ppacs pacsdb < /root/create.mysql
 # Load the 'arrdb' database schema
 mysql -uarr -parr arrdb < /root/dcm4chee-arr-mysql.ddl
+sleep 240s
 killall mysqld
 sleep 5s
 chown -R nobody:users /DATABASE
+fi
