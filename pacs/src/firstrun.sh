9314c291b8b51bf5ca7f7a8d7f39c9e1b58cd8d1
sparklyballs <sparklyballs@gmail.com>
2015-03-21 11:45:34 +0000

    Update firstrun.sh

--- pacs/src/firstrun.sh
+++ pacs/src/firstrun.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-if [ -f "/DICOM/dcm4chee/dcm4chee-mysql/bin/run.sh" ]; then
+if [ -f "/config/dcm4/dcm4chee/dcm4chee-mysql/bin/run.sh" ]; then
 echo "using existing setup"
-chown -R nobody:users /DICOM
+chown -R nobody:users /config
 else
 echo "creating new setup"
-cp -pr /root/temp-setup/dcm4chee/ /DICOM/
-chown -R nobody:users /DICOM
+cp -pr /root/temp-setup/dcm4chee/ /config/dcm4/
+chown -R nobody:users /config
 sleep 10
 fi
 
 rm -rf /var/local/mysql/*.pid
-if [ -d "/DATABASE/arrdb" ]; then
+if [ -d "/config/databases/arrdb" ]; then
 echo "Using existing databases"
-chown -R nobody:users /DATABASE
+chown -R nobody:users /config
 /usr/bin/supervisord
 sleep 20s
 else
 echo "new setup, copying empty databases"
-cp -pr /var/lib/mysql/* /DATABASE/
-chown -R nobody:users /DATABASE
+cp -pr /var/lib/mysql/* /config/databases/
+chown -R nobody:users /config
 sleep 10s
 /usr/bin/supervisord
 sleep 20s
