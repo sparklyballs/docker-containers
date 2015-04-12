5373c85db500d71aea17f7ad01fd204e6dfc1cb6
sparklyballs <sparklyballs@gmail.com>
2015-04-12 19:37:06 +0100

    Update start.sh

--- sonarr/develop/start.sh
+++ sonarr/develop/start.sh
@@ -9,5 +9,5 @@ function handle_signal {
 trap "handle_signal" SIGINT SIGTERM SIGHUP
 
 echo "starting sonarr"
-mono /opt/NzbDrone/NzbDrone.exe --no-browser -data=/volumes/config/sonarr & wait
+mono /opt/NzbDrone/NzbDrone.exe --no-browser -data=/config/sonarr & wait
 echo "stopping sonarr"
