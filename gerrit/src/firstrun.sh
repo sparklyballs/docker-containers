9bc7d443e439f8c79f52be6348b472bc1aca6a82
sparklyballs <sparklyballs@gmail.com>
2015-04-13 18:39:46 +0100

    Update firstrun.sh

--- gerrit/src/firstrun.sh
+++ gerrit/src/firstrun.sh
@@ -2,6 +2,8 @@
 chown -R nobody:users /opt/gerrit
 if [ -d "/opt/gerrit/bin" ]; then
 echo "gerrit files already in place"
+chown -R nobody:users /opt/gerrit
+exec /usr/bin/supervisord
 else
 /sbin/setuser nobody java -jar $GERRIT_WAR init --batch -d $GERRIT_HOME
 cp /root/gerrit.config /opt/gerrit/etc/gerrit.config
