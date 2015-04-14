a375166ca2209db71117ad10a96cc3163861af2a
sparklyballs <sparklyballs@gmail.com>
2015-04-15 00:50:10 +0100

    Update edge.sh

--- htpcmanager/edge.sh
+++ htpcmanager/edge.sh
@@ -3,6 +3,6 @@
 if [ -z "$EDGE" ]; then
 echo "edge not requested"
 else
-rm -rf /opt/HTPC-Manager
-git clone -b master https://github.com/styxit/HTPC-Manager.git /opt/HTPC-Manager
+cd /opt/HTPC-Manager
+git pull
 fi
