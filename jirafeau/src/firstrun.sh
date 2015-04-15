e5a3ba3931feddfb1c92d5e362faad289752bd26
sparklyballs <sparklyballs@gmail.com>
2015-04-15 02:41:41 +0100

    Update firstrun.sh

--- jirafeau/src/firstrun.sh
+++ jirafeau/src/firstrun.sh
@@ -3,7 +3,7 @@ if [ -f "/var/www/Jirafeau/index.php" ]; then
 echo "using existing website"
 chown -R www-data:www-data /var/log/apache2
 else
-echo "fetching freshress files"
+echo "fetching jirafeau files"
 git clone https://gitlab.com/mojo42/Jirafeau.git /var/www/Jirafeau
 chown -R www-data:www-data /var/www/Jirafeau
 fi
