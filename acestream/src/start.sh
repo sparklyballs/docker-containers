119e62fd75dd698e24acb8b3e3fb7e8e81c130fc
sparklyballs <sparklyballs@gmail.com>
2015-04-17 20:39:42 +0100

    Update start.sh

--- acestream/src/start.sh
+++ acestream/src/start.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-HOST_IP="$(hostname -I | sed 's/ *$//')"
+
 
 cat > /home/tv/aceproxy-master/plugins/config/torrenttv.py << EOF
 url = '$TTV_URL'
 updateevery = 0
 EOF
 
-echo "Paste this URL into your player"
-echo "http://$HOST_IP:8000/torrenttv/torrenttv.m3u"
+
 
 
 sed -i 's/vlcuse = False/vlcuse = True/' /home/tv/aceproxy-master/aceconfig.py
