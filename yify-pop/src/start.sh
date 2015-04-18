9f07061731d7f453c48c8aaac5ee7df6f66d97fd
sparklyballs <sparklyballs@gmail.com>
2015-04-18 13:36:58 +0100

    Update start.sh

--- yify-pop/src/start.sh
+++ yify-pop/src/start.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-mkdir -p /opt/yify-pop
-
-if [ -f "/opt/yify-pop/app" ]; then
-echo "yify-pop files are already in place"
-else
-cp -pr /root/yify-pop/* /opt/yify-pop/
-cd /opt/yify-pop
-npm install
-sleep 10s
-fi
-
 exec /usr/bin/supervisord
