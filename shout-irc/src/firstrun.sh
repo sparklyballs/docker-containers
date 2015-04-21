535ad96f3c0fa9021c453195ade053349cfe3d7a
sparklyballs <sparklyballs@gmail.com>
2015-04-21 13:40:45 +0100

    Update firstrun.sh

--- shout-irc/src/firstrun.sh
+++ shout-irc/src/firstrun.sh
@@ -4,7 +4,7 @@ if [ -f "/config/config.js" ]; then
 echo "using saved config file"
 else
 cp /usr/local/lib/node_modules/shout/defaults/config.js /config/config.js
-chown nobody:users /config/config.js
-chmod 777 /config/config.js
+chown -R nobody:users /config/config.js
+chmod -R 777 /config/config.js
 sleep 5s
 fi
