bfa3886fff0f42dddbde5877dc656622367c0eb8
sparklyballs <sparklyballs@gmail.com>
2015-04-21 05:56:24 +0100

    Delete start.sh

--- shout-irc/src/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -d "/usr/local/lib/node_modules/shout/defaults" ]; then
-echo "shout is already installed"
-else
-npm install -g shout
-chown -R nobody:users /usr/local/lib/node_modules/shout
-fi
