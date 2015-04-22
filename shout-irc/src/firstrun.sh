238b75dadf38fa9d66eb821f56030a3de6515392
sparklyballs <sparklyballs@gmail.com>
2015-04-22 12:29:23 +0100

    changed config.js shout-irc

--- shout-irc/src/firstrun.sh
+++ shout-irc/src/firstrun.sh
@@ -3,7 +3,7 @@
 if [ -f "/config/config.js" ]; then
 echo "using saved config file"
 else
-cp /usr/local/lib/node_modules/shout/defaults/config.js /config/config.js
+cp /root/config.js /config/config.js
 chown -R nobody:users /config
 sleep 5s
 fi
