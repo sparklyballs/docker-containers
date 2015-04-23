4cd3411e4f252523e45e1892f5e96c92e20110a0
sparklyballs <sparklyballs@gmail.com>
2015-04-23 15:51:33 +0100

    second fix up simplehelp

--- simplehelp/src/start.sh
+++ simplehelp/src/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-exec /sbin/setuser nobody /opt/SimpleHelp/jre1.6.0_16/bin/java -Xmx384m -Djava.net.preferIPv4Stack=true -cp "lib/secure_shelp.jar:lib/secure_utils.jar:lib/secure_nlink.jar:lib/mail.jar:lib/activation.jar" -Djava.awt.headless=true SecureRunner1 com.aem.shelp.proxy.ProxyServerStartup &
+exec /sbin/setuser nobody /opt/SimpleHelp//jre1.6.0_16/bin/java -Xmx384m -Djava.net.preferIPv4Stack=true -cp "/opt/SimpleHelp//lib/secure_shelp.jar:/opt/SimpleHelp//lib/secure_utils.jar:/opt/SimpleHelp//lib/secure_nlink.jar:/opt/SimpleHelp//lib/mail.jar:/opt/SimpleHelp//lib/activation.jar" -Djava.awt.headless=true SecureRunner1 com.aem.shelp.proxy.ProxyServerStartup &
+
