d22188fd8356230092d154761c5064101fefa01e
sparklyballs <sparklyballs@gmail.com>
2015-04-23 03:47:32 +0100

    add magpie

---
+++ magpie/src/firstrun.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -d "/config/git/.git" ]; then
+echo "save area already configured"
+else 
+mkdir -p /config
+cd /config
+git init
+chown -R nobody:users /config
+mv /usr/local/lib/python2.7/dist-packages/magpie/config/web.cfg /usr/local/lib/python2.7/dist-packages/magpie/config/web.original
+cp /root/web.cfg /usr/local/lib/python2.7/dist-packages/magpie/config/web.cfg
+fi
+
