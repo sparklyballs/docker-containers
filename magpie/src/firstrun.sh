7d963922a36d9912c58c3896fe2590b9ed1a4fd9
sparklyballs <sparklyballs@gmail.com>
2015-04-23 11:41:03 +0100

    Update firstrun.sh

--- magpie/src/firstrun.sh
+++ magpie/src/firstrun.sh
@@ -7,8 +7,13 @@ mkdir -p /config
 cd /config
 git init
 chown -R nobody:users /config
+fi
+
+if [ -f "/usr/local/lib/python2.7/dist-packages/magpie/config/web.original"]
+echo "config is set"
+else
 mv /usr/local/lib/python2.7/dist-packages/magpie/config/web.cfg /usr/local/lib/python2.7/dist-packages/magpie/config/web.original
 cp /root/web.cfg /usr/local/lib/python2.7/dist-packages/magpie/config/web.cfg
 chown -R nobody:users /usr/local/lib/python2.7/dist-packages/magpie
 fi
-
+sleep 2s
