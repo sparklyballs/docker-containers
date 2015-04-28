ba0bc1a6547c7dc65a0f9711e6242e0ddf04e8a2
sparklyballs <sparklyballs@gmail.com>
2015-04-28 05:13:31 +0100

    Update firstrun.sh

--- pydio/src/firstrun.sh
+++ pydio/src/firstrun.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 mkdir -p /var/www/pydio
+mkdir -p /config
+
 if [ -f "/var/www/pydio/index.php" ]; then
 echo "using existing website"
 chown -R www-data:www-data /var/log/apache2
@@ -9,4 +11,12 @@ cp -r /root/pydio/* /var/www/pydio/
 chown -R www-data:www-data /var/www/pydio/data
 fi
 
+if [ -f "/config/ssmtp.conf" ]; then
+rm /etc/ssmtp/ssmtp.conf
+cp /config/ssmtp.conf /etc/ssmtp/ssmtp.conf
+else
+cp /root/ssmtp.conf /config/ssmtp.conf
+chown -R nobody:users /config
+echo "EDIT SSMTP CONFIG AND RESTART CONTAINER"
+fi
 
