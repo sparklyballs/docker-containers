1852fede05aeba6aec9a8522a042b7daea424ab4
sparklyballs <sparklyballs@gmail.com>
2015-05-18 23:13:21 +0100

    Update firstrun.sh

--- serviio/src/firstrun.sh
+++ serviio/src/firstrun.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-
+mkdir -p /var/www/serviio
 if [ -f "/var/www/serviio/index.php" ]; then
 echo "using existing website"
 chown -R www-data:www-data /var/log/apache2
 else
 echo "fetching Serviio WebUI files"
+cd /tmp
 wget https://github.com/SwoopX/Web-UI-for-Serviio/archive/Serviio-1.5.zip
 unzip Serviio-1.5.zip
 mv Web-UI-for-Serviio-Serviio-1.5 serviio
+mv serviio/* /var/www/serviio/
 chown -R www-data:www-data /var/www/serviio /var/log/apache2
 fi
 
