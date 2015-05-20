7e76f2bbb7b6d24a8f26d8492852e5a1ebb32d79
sparklyballs <sparklyballs@gmail.com>
2015-05-20 16:07:56 +0100

    Update firstrun.sh

--- serviio/src/firstrun.sh
+++ serviio/src/firstrun.sh
@@ -2,6 +2,7 @@
 mkdir -p /opt/serviio
 if [ -d "/opt/serviio/config" ]; then
 echo "using existing serviio files"
+usr/bin/supervisord -c /root/supervisord.conf &
 else
 cp -pr /root/serviio/* /opt/serviio/
 usr/bin/supervisord -c /root/supervisord.conf &
