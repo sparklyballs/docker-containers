adb6ea76d3c056bfbbc1b7572ae7426508488ca6
sparklyballs <sparklyballs@gmail.com>
2015-05-20 12:07:32 +0100

    Update updatedb.sh

--- musicbrainz/src/updatedb.sh
+++ musicbrainz/src/updatedb.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-apt-get install -y wget
 rm -rf /import/*
 wget -nd -nH -P /import ftp://ftp.musicbrainz.org/pub/musicbrainz/data/fullexport/LATEST
-LATEST=$(cat /media/dbdump/LATEST)
+LATEST=$(cat /import/LATEST)
 wget -r --no-parent -nd -nH -P /import --reject "index.html*, mbdump-edit*, mbdump-documentation*" "ftp://ftp.musicbrainz.org/pub/musicbrainz/data/fullexport/$LATEST"
 pushd /import && md5sum -c MD5SUMS && popd
-/opt/musicbrainz/admin/InitDb.pl --createdb --import /media/dbdump/mbdump*.tar.bz2 --echo
-
+/opt/musicbrainz/admin/InitDb.pl --createdb --import /import/mbdump*.tar.bz2 --echo
