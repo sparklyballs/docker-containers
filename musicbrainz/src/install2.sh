f66a7c16a550aca9ef38499c92b5a38156b7c57d
sparklyballs <sparklyballs@gmail.com>
2015-05-20 20:09:16 +0100

    Update install2.sh

--- musicbrainz/src/install2.sh
+++ musicbrainz/src/install2.sh
@@ -111,7 +111,7 @@ cat <<'EOT' > /etc/my_init.d/004-import-databases--and-or-run-everything.sh
 #!/bin/bash
 crontab /root/cronjob
 cd /opt/musicbrainz
-exec plackup -Ilib -r
+plackup -Ilib -r
 EOT
 
 
