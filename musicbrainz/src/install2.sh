d72e42b0dc3cbeb86089a933a15bda57fdc41307
sparklyballs <sparklyballs@gmail.com>
2015-05-20 09:31:58 +0100

    Update install2.sh

--- musicbrainz/src/install2.sh
+++ musicbrainz/src/install2.sh
@@ -72,6 +72,13 @@ sleep 5s
 sleep 10s
 /sbin/setuser postgres psql --command="CREATE USER musicbrainz WITH SUPERUSER PASSWORD 'musicbrainz';" >/dev/null 2>&1
 sleep 5s
+echo "BEGINNING INITIAL DATABASE IMPORT ROUTINE, THIS WILL TAKE SEVERAL HOURS, DO NOT STOP DOCKER UNTIL IT IS COMPLETED"
+rm -rf /import/*
+wget -nd -nH -P /import ftp://ftp.musicbrainz.org/pub/musicbrainz/data/fullexport/LATEST
+LATEST=$(cat /media/dbdump/LATEST)
+wget -r --no-parent -nd -nH -P /import --reject "index.html*, mbdump-edit*, mbdump-documentation*" "ftp://ftp.musicbrainz.org/pub/musicbrainz/data/fullexport/$LATEST"
+pushd /import && md5sum -c MD5SUMS && popd
+/opt/musicbrainz/admin/InitDb.pl --createdb --import /media/dbdump/mbdump*.tar.bz2 --echo
 fi
 EOT
 
