dc14f7875a1e1891d4f9c837fa3f27df0541ae06
sparklyballs <sparklyballs@gmail.com>
2015-05-20 01:40:29 +0100

    Update start.sh

--- serviio/src/start.sh
+++ serviio/src/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /opt/serviio/bin
+/usr/lib/jvm/java-8-oracle/bin/java -Xmx512M -Xms20M -XX:+UseG1GC -XX:GCTimeRatio=1 -XX:MinHeapFreeRatio=10 -XX:MaxHeapFreeRatio=20 -Djava.net.preferIPv4Stack=true -Djava.awt.headless=true -Dorg.restlet.engine.loggerFacadeClass=org.restlet.ext.slf4j.Slf4jLoggerFacade -Dderby.system.home=/opt/serviio/library -Dserviio.home=/opt/serviio -Dffmpeg.location=/usr/bin/ffmpeg -Ddcraw.location=/usr/bin/dcraw -classpath /opt/serviio/lib/*:/opt/serviio/config org.serviio.MediaServer
 
