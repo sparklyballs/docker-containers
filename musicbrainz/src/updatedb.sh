fdbaea9cc141efcbd48a1199f034a02308e5dd34
sparklyballs <sparklyballs@gmail.com>
2015-05-22 13:40:24 +0100

    clear some build dev cruff

--- musicbrainz/src/updatedb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -rf /import/*
-wget -nd -nH -P /import ftp://ftp.musicbrainz.org/pub/musicbrainz/data/fullexport/LATEST
-LATEST=$(cat /import/LATEST)
-wget -r --no-parent -nd -nH -P /import --reject "index.html*, mbdump-edit*, mbdump-documentation*" "ftp://ftp.musicbrainz.org/pub/musicbrainz/data/fullexport/$LATEST"
-pushd /import && md5sum -c MD5SUMS && popd
-/opt/musicbrainz/admin/InitDb.pl --createdb --import /import/mbdump*.tar.bz2 --echo
