7cedcae9305786519ec10254dc7dd1c8a20b3525
sparklyballs <sparklyballs@gmail.com>
2015-05-25 16:51:27 +0100

    Update couchpotato.sh

--- couchpotato/couchpotato.sh
+++ couchpotato/couchpotato.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec /sbin/setuser nobody python /opt/couchpotato/CouchPotato.py --config_file=/config/settings.conf --data_dir=/config/data
+exec /sbin/setuser nobody python /opt/couchpotato/CouchPotato.py --config_file=/config/settings.conf --data_dir=/config/data > /dev/null 2>&1
