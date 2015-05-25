48782fee750b864c1bc1bf55d212db89330f3dc0
sparklyballs <sparklyballs@gmail.com>
2015-05-25 16:52:25 +0100

    Update edge.sh

--- couchpotato/edge.sh
+++ couchpotato/edge.sh
@@ -5,6 +5,8 @@ if [ -z "$EDGE" ]; then
   echo "Bleeding edge not requested"
 else
   cd /opt/couchpotato
-  git pull
+  echo "pulling updates from git"
+  git pull > /dev/null 2>&1
+  echo "pull complete"
   chown -R nobody:users /opt/couchpotato
 fi
