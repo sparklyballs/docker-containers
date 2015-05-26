20394001906c414c07ffc752c83734f2c946a61b
sparklyballs <sparklyballs@gmail.com>
2015-05-26 11:00:29 +0100

    Update 002-postgres-initialise.sh

--- pynab/src/002-postgres-initialise.sh
+++ pynab/src/002-postgres-initialise.sh
@@ -11,9 +11,13 @@ sed -i '/^hba_file*/ s|/etc/postgresql/9.4/main/pg_hba.conf|/data/pg_hba.conf|' 
 mkdir -p /data/main
 chown postgres:postgres /data/*
 chmod 700 /data/main
-/sbin/setuser postgres /usr/lib/postgresql/9.4/bin/initdb -D /data/main
+echo "initialising empty databases in /data"
+/sbin/setuser postgres /usr/lib/postgresql/9.4/bin/initdb -D /data/main >/dev/null 2>&1
+echo "completed initialisation"
 sleep 5s
 /usr/bin/supervisord -c /root/postgres-supervisord.conf &
 sleep 10s
+/sbin/setuser postgres psql --command='CREATE ROLE "www-data" superuser;' >/dev/null 2>&1
+sleep 5s
 fi
 
