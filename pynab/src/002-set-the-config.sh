440a9a82e05435062ea1b38a5c3012b10561c999
sparklyballs <sparklyballs@gmail.com>
2015-05-27 12:36:02 +0100

    fix up pynab config start routine

--- pynab/src/002-set-the-config.sh
+++ pynab/src/002-set-the-config.sh
@@ -4,8 +4,8 @@ if [ -f "/config/config.py" ]; then
 echo "config files exist in /config, may require editing"
 cp /config/config.py /opt/pynab/
 else
-cp /root/config-files /config/config.py
+cp /root/config-files/config.py /config/config.py
 fi
-cp /config/config.py /opt/pynab/
+cp /config/config.py /opt/pynab/config.py
 chown nobody:users /config/config.py
 chown -R www-data:www-data /opt/pynab
