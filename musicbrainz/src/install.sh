cba2d370918b73e0e5d537343784029c7b89d9c0
sparklyballs <sparklyballs@gmail.com>
2015-06-01 14:31:13 +0100

    set hot standy on postgres
    allow transactions while repairing from unclean shutdown.

--- musicbrainz/src/install.sh
+++ musicbrainz/src/install.sh
@@ -169,6 +169,7 @@ cp /etc/postgresql/9.4/main/postgresql.conf /data/postgresql.conf
 cp /etc/postgresql/9.4/main/pg_hba.conf /data/pg_hba.conf
 sed -i '/^data_directory*/ s|/var/lib/postgresql/9.4/main|/data/main|' /data/postgresql.conf
 sed -i '/^hba_file*/ s|/etc/postgresql/9.4/main/pg_hba.conf|/data/pg_hba.conf|' /data/postgresql.conf
+echo "hot_standby = on" >> /data/postgresql.conf
 mkdir -p /data/main
 chown postgres:postgres /data/*
 chmod 700 /data/main
