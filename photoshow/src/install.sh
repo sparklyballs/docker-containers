1b19c64708f12d13dda2eb45c304e03a9333d8ee
sparklyballs <sparklyballs@gmail.com>
2015-06-04 08:25:01 +0100

    Update install.sh

--- photoshow/src/install.sh
+++ photoshow/src/install.sh
@@ -97,11 +97,11 @@ if [[ $(cat /etc/timezone) != $TZ ]] ; then
   echo "$TZ" > /etc/timezone
  exec  dpkg-reconfigure -f noninteractive tzdata
 fi
-sed -i -e 's/^#//' /var/www/PhotoShow/config.php
-sed -i -e "s@\$config->timezone.*@\$config->timezone = \"${TZ}\"@g" /var/www/PhotoShow/config.php
 EOT
 
 cat <<'EOT' > /etc/my_init.d/002-start-stuff.sh
 #!/bin/bash
+sed -i -e 's/^#//' /var/www/PhotoShow/config.php
+sed -i -e "s@\$config->timezone.*@\$config->timezone = \"${TZ}\"@g" /var/www/PhotoShow/config.php
 /usr/bin/supervisord -c /root/photoshow.conf &
 EOT
