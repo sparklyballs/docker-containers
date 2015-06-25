2c522bc95ee50b3f851148764e813547f57dcf96
sparklyballs <sparklyballs@gmail.com>
2015-06-25 10:09:13 +0100

    Update firstrun.sh

--- usenet-pack/src/firstrun.sh
+++ usenet-pack/src/firstrun.sh
@@ -4,21 +4,21 @@ if [ -d "/config/couchpotato" ]; then
 echo "folders appear set"
 else
 mkdir -p /config/couchpotato /config/sabnzbd /config/sickbeard /downloads/movies /downloads/tv-shows
-SAPAPI_KEY=$(< /dev/urandom tr -dc a-z0-9  | head -c${1:-32})
+SAB_API_KEY=$(< /dev/urandom tr -dc a-z0-9  | head -c${1:-32})
 fi
 
 if [ -f "/config/sabnzbd/sabnzbd.ini" ]; then
 echo "sabnzbd config file appears to be set"
 else
 cp /root/config-files/sabnzbd.ini  /config/sabnzbd/sabnzbd.ini
-sed -i  "/create_group_folders = 0/{n;s/.*/api_key =${SAPAPI_KEY}/}" /config/sabnzbd/sabnzbd.ini
+sed -i  "/create_group_folders = 0/{n;s/.*/api_key =${SAB_API_KEY}/}" /config/sabnzbd/sabnzbd.ini
 fi
 
 if [ -f "/config/sickbeard/config.ini" ]; then
 echo "sickbeard config file appears set"
 else
 cp /root/config-files/config.ini /config/sickbeard/config.ini
-sed -i -e "s@sab_apikey.*@sab_apikey = \"${SAPAPI_KEY}\"@g" /config/sickbeard/config.ini
+sed -i -e "s@sab_apikey.*@sab_apikey = \"${SAB_API_KEY}\"@g" /config/sickbeard/config.ini
 fi
 
 if [ -f "/config/couchpotato/settings.conf" ]; then
