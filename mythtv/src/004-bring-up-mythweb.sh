c1010f15c275e320a657ddfac9dc506658af88d4
sparklyballs <sparklyballs@gmail.com>
2015-06-27 17:42:33 +0100

    Update 004-bring-up-mythweb.sh

--- mythtv/src/004-bring-up-mythweb.sh
+++ mythtv/src/004-bring-up-mythweb.sh
@@ -4,6 +4,6 @@ if [ -d "/var/lib/mythtv/banners" ]; then
 echo "mythtv folders appear to be set"
 else
 mkdir -p /var/lib/mythtv/banners  /var/lib/mythtv/coverart  /var/lib/mythtv/db_backups  /var/lib/mythtv/fanart  /var/lib/mythtv/livetv  /var/lib/mythtv/recordings  /var/lib/mythtv/screenshots  /var/lib/mythtv/streaming  /var/lib/mythtv/trailers  /var/lib/mythtv/videos
-chown -R mythtv:users /var/lib/mythtv
+chown -R mythtv:users /var/lib/mythtv/banners  /var/lib/mythtv/coverart  /var/lib/mythtv/db_backups  /var/lib/mythtv/fanart  /var/lib/mythtv/livetv  /var/lib/mythtv/recordings  /var/lib/mythtv/screenshots  /var/lib/mythtv/streaming  /var/lib/mythtv/trailers  /var/lib/mythtv/videos
 fi
 exec /usr/sbin/apache2ctl -D FOREGROUND
