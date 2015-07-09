21d86cac8680b75f47ed945bdedccb485aa937ab
sparklyballs <sparklyballs@gmail.com>
2015-07-09 12:51:44 +0100

    Update 001-bring-up-the-app.sh

--- daapd-server/src/001-bring-up-the-app.sh
+++ daapd-server/src/001-bring-up-the-app.sh
@@ -3,4 +3,4 @@ if [ ! -f "/config/forked-daapd.conf" ]; then
 cp /root/forked-daapd.conf /config/forked-daapd.conf
 fi
 chown nobody:users /config/forked-daapd.conf
-exec /usr/bin/supervisord -c /root/supervisord.conf > /dev/null 2>&1 &
+exec /usr/bin/supervisord -c /root/supervisord.conf & > /dev/null 2>&1 
