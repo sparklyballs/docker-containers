0a698c052276a2c7472de4bd2a06a2f0a801c40a
sparklyballs <sparklyballs@gmail.com>
2015-07-09 08:14:53 +0100

    first unraid test daapd server

---
+++ daapd-server/src/001-bring-up-the-app.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [ ! -f "/config/forked-daapd.conf" ]; then
+cp /root/forked-daapd.conf /config/forked-daapd.conf
+fi
+chown nobody:users /config/forked-daapd.conf
+exec /usr/bin/supervisord -c /root/supervisord.conf
