71a7dc4260c1196b9224ca8a5c89c5b9cfbd46ca
sparklyballs <sparklyballs@gmail.com>
2015-07-10 22:33:48 +0100

    complete rebuild of daapd server with compiled dependencies

--- daapd-server/src/001-bring-up-the-app.sh
+++ daapd-server/src/001-bring-up-the-app.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-if [ ! -f "/config/avahi-daemon.conf" ]; then
-cp /root/avahi-daemon.conf /config/avahi-daemon.conf
-fi
 if [ ! -f "/config/forked-daapd.conf" ]; then
 cp /root/forked-daapd.conf /config/forked-daapd.conf
 fi
@@ -9,5 +6,5 @@ if [ ! -d "/config/logs-databases-and-cache" ]; then
 mkdir -p /config/logs-databases-and-cache
 fi
 chown -R root:root /config/logs-databases-and-cache
-chown nobody:users /config/forked-daapd.conf /config/avahi-daemon.conf
+chown nobody:users /config/forked-daapd.conf
 exec /usr/bin/supervisord -c /root/supervisord.conf & > /dev/null 2>&1 
