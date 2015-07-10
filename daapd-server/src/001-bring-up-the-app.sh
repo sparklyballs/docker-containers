3681f58c223292582f30d589209af991a452464e
sparklyballs <sparklyballs@gmail.com>
2015-07-10 11:17:32 +0100

    Update 001-bring-up-the-app.sh

--- daapd-server/src/001-bring-up-the-app.sh
+++ daapd-server/src/001-bring-up-the-app.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [ ! -f "/config/avahi-daemon.conf" ]; then
+cp /root/avahi-daemon.conf /config/avahi-daemon.conf
+fi
 if [ ! -f "/config/forked-daapd.conf" ]; then
 cp /root/forked-daapd.conf /config/forked-daapd.conf
 fi
@@ -6,5 +9,5 @@ if [ ! -d "/config/logs-databases-and-cache" ]; then
 mkdir -p /config/logs-databases-and-cache
 fi
 chown -R root:root /config/logs-databases-and-cache
-chown nobody:users /config/forked-daapd.conf
+chown nobody:users /config/forked-daapd.conf /config/avahi-daemon.conf
 exec /usr/bin/supervisord -c /root/supervisord.conf & > /dev/null 2>&1 
