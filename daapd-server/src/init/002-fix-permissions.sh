426adbc75dd26f1a54c2e5bf053b8e874b56b0bc
sparklyballs <sparklyballs@gmail.com>
2015-07-11 16:31:20 +0100

    Delete 002-fix-permissions.sh

--- daapd-server/src/init/002-fix-permissions.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-PUID=99
-PGID=100
-if [ ! "$(id -u daapd)" -eq "$PUID" ]; then usermod -u "$PUID" daapd; fi
-if [ ! "$(id -g daapd)" -eq "$PGID" ]; then groupmod -o -g "$PGID" daapd ; fi
