0ca0ff7f459af95101c0c0b1cb908c2cffa4fadd
sparklyballs <sparklyballs@gmail.com>
2015-07-11 12:30:07 +0100

    Update 002-set-config-files.sh

--- daapd-server/src/002-set-config-files.sh
+++ daapd-server/src/002-set-config-files.sh
@@ -4,9 +4,11 @@ cp /root/forked-daapd.conf /config/forked-daapd.conf
 fi
 if [ ! -d "/config/logs-databases-and-cache" ]; then
 mkdir -p /config/logs-databases-and-cache
+chown -R nobody:users /config/logs-databases-and-cache
 fi
 if [ ! -d "/daapd-pidfolder" ]; then
 mkdir -p /daapd-pidfolder
 fi
-chown -R nobody:users /config /daapd-pidfolder
+chown nobody:users /config/forked-daapd.conf
+chown -R nobody:users /daapd-pidfolder
 
