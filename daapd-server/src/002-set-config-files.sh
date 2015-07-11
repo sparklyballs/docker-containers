3485d414bb4be53c4fd06d97ae5df5aff3b525d7
sparklyballs <sparklyballs@gmail.com>
2015-07-11 01:45:41 +0100

    Create 002-set-config-files.sh

---
+++ daapd-server/src/002-set-config-files.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ ! -f "/config/forked-daapd.conf" ]; then
+cp /root/forked-daapd.conf /config/forked-daapd.conf
+fi
+if [ ! -d "/config/logs-databases-and-cache" ]; then
+mkdir -p /config/logs-databases-and-cache
+fi
+chown -R root:root /config/logs-databases-and-cache
+chown nobody:users /config/forked-daapd.conf
