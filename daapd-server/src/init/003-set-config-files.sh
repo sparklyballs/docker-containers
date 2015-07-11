431a934ebaea42310062b9a34b3686d49d82883c
sparklyballs <sparklyballs@gmail.com>
2015-07-11 14:45:47 +0100

    bump libav to 11 daapd-server and try permissions change AGAIN git add -A .

--- daapd-server/src/init/003-set-config-files.sh
+++ daapd-server/src/init/003-set-config-files.sh
@@ -4,11 +4,11 @@ cp /root/forked-daapd.conf /config/forked-daapd.conf
 fi
 if [ ! -d "/config/logs-databases-and-cache" ]; then
 mkdir -p /config/logs-databases-and-cache
-chown -R nobody:users /config/logs-databases-and-cache
+chown -R daapd:daapd /config/logs-databases-and-cache
 fi
 if [ ! -d "/daapd-pidfolder" ]; then
 mkdir -p /daapd-pidfolder
 fi
-chown nobody:users /config/forked-daapd.conf
-chown -R nobody:users /daapd-pidfolder
+chown daapd:daapd /config/forked-daapd.conf
+chown -R daapd:daapd /daapd-pidfolder
 
