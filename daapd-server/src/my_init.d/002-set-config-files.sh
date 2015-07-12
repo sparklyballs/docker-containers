c4852587f37ca6cf7786a3eece55ce5e0f04bc57
sparklyballs <sparklyballs@gmail.com>
2015-07-12 17:11:12 +0100

    clean root and build cruft daapd-server

--- daapd-server/src/my_init.d/002-set-config-files.sh
+++ daapd-server/src/my_init.d/002-set-config-files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -f "/config/forked-daapd.conf" ]; then
-cp /root/forked-daapd.conf /config/forked-daapd.conf
+cp /root/config-files/forked-daapd.conf /config/forked-daapd.conf
 fi
 if [ ! -d "/config/logs-databases-and-cache" ]; then
 mkdir -p /config/logs-databases-and-cache
