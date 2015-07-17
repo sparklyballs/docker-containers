9588038c4228072a776e5f59fb52fdabe1013ce4
sparklyballs <sparklyballs@gmail.com>
2015-07-17 12:51:46 +0100

    Update firstrun.sh

--- wine/src/firstrun.sh
+++ wine/src/firstrun.sh
@@ -7,8 +7,8 @@ fi
 
 mkdir -p /home/ubuntu
 chown -R ubuntu:users /home/ubuntu/
-mkdir  -p /var/run/sshd
-mkdir  -p /root/.vnc
+mkdir /var/run/sshd
+mkdir  /root/.vnc
 /usr/bin/supervisord -c /root/supervisord.conf
 while [ 1 ]; do
 /bin/bash
