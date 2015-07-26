e43f42c5fd62fcc568c039a0718a6523f6ead589
sparklyballs <sparklyballs@gmail.com>
2015-07-26 06:10:14 +0100

    Update 003-bring-the-app-up.sh

--- minetest/src/init/003-bring-the-app-up.sh
+++ minetest/src/init/003-bring-the-app-up.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec /sbin/setuser minetest /usr/local/bin/minetestserver --gameid minimal --config /home/minetest/.minetest/minetest.conf
+exec /sbin/setuser minetest /usr/local/bin/minetestserver --port 30000 --config /home/minetest/.minetest/minetest.conf --gameid minimal
